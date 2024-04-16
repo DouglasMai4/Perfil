a110fadb30b18fd6815ae568b6631809263938d5
Douglas Maia <douglasmai4@protonmail.com>
2024-04-16 16:32:49 -0300

    feat: Install astro icon dependencie

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 
+import icon from "astro-icon";
+
 // https://astro.build/config
 export default defineConfig({
-    image: {
-        domains: ['github.com']
-    }
+  image: {
+    domains: ['github.com']
+  },
+  integrations: [icon()]
 });
